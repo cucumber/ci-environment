a3b6adf89051fc6bcedb2b828e1334ed04d3a437
aurelien-reeves <aurelien.reeves@hiptest.net>
2021-11-16 09:57:17 +0100

    Copy ciDict.json to ruby implementation

--- ruby/lib/cucumber/create_meta.rb
+++ ruby/lib/cucumber/create_meta.rb
@@ -7,25 +7,25 @@ require 'cucumber/create_meta/variable_expression'
 module Cucumber
   module CreateMeta
     extend Cucumber::CreateMeta::VariableExpression
-    CI_DICT = JSON.parse(IO.read(File.join(File.dirname(__FILE__), '..', '..', 'ciDict.json')))
+    CI_DICT = JSON.parse(IO.read(File.join(File.dirname(__FILE__), 'ciDict.json')))
 
     def create_meta(tool_name, tool_version, env = ENV)
       Cucumber::Messages::Meta.new(
         protocol_version: Cucumber::Messages::VERSION,
         implementation: Cucumber::Messages::Product.new(
-            name: tool_name,
-            version: tool_version
+          name: tool_name,
+          version: tool_version
         ),
         runtime: Cucumber::Messages::Product.new(
-            name: RUBY_ENGINE,
-            version: RUBY_VERSION
+          name: RUBY_ENGINE,
+          version: RUBY_VERSION
         ),
         os: Cucumber::Messages::Product.new(
-            name: RbConfig::CONFIG['target_os'],
-            version: Sys::Uname.uname.version
+          name: RbConfig::CONFIG['target_os'],
+          version: Sys::Uname.uname.version
         ),
         cpu: Cucumber::Messages::Product.new(
-            name: RbConfig::CONFIG['target_cpu']
+          name: RbConfig::CONFIG['target_cpu']
         ),
         ci: detect_ci(env)
       )
@@ -51,18 +51,19 @@ module Cucumber
           remote: remove_userinfo_from_url(evaluate(ci_system['git']['remote'], env)),
           revision: evaluate(ci_system['git']['revision'], env),
           branch: evaluate(ci_system['git']['branch'], env),
-          tag: evaluate(ci_system['git']['tag'], env),
+          tag: evaluate(ci_system['git']['tag'], env)
         )
       )
     end
 
     def remove_userinfo_from_url(value)
       return nil if value.nil?
+
       begin
         uri = URI(value)
         uri.userinfo = ''
         uri.to_s
-      rescue
+      rescue StandardError
         value
       end
     end
