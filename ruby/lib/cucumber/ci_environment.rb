f98e67423b6a57dd8a3f8a99fef0dd7ddc9c95d9
Charles Alderete <charlie.alderete@coxautoinc.com>
2023-12-22 13:09:12 -0500

    refactored detect revision method to use dig()

--- ruby/lib/cucumber/ci_environment.rb
+++ ruby/lib/cucumber/ci_environment.rb
@@ -63,8 +63,8 @@ module Cucumber
 
         return revision
       end
-
-      evaluate(ci_environment['git']['revision'], env)
+      
+      evaluate(ci_environment.dig('git', 'revision'), env)
     end
 
     def remove_userinfo_from_url(value)
