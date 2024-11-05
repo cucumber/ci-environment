62fba0111ea50ac460368bf8e321eba26678f3e1
Luke Hill <lukehill_uk@hotmail.com>
2024-11-05 10:39:17 +0000

    Fix up a load of offenses generally in ci_environment_spec

--- ruby/spec/cucumber/ci_environment/ci_environment_spec.rb
+++ ruby/spec/cucumber/ci_environment/ci_environment_spec.rb
@@ -4,25 +4,23 @@ require 'cucumber/ci_environment'
 require 'json'
 
 describe 'detect_ci_environment' do
+  subject { JSON.parse(ci_environment.to_json) }
+
+  let(:ci_environment) { Cucumber::CiEnvironment.detect_ci_environment(env) }
+
   Dir.glob('../testdata/*.txt') do |test_data_file|
     context "with #{File.basename(test_data_file, '.txt')}" do
-      subject { JSON.parse(ci_environment.to_json) }
-
-      let(:ci_environment) { Cucumber::CiEnvironment.detect_ci_environment(env) }
-      let(:env) { Hash[entries] }
       let(:entries) { env_data.split("\n").map { |line| line.split('=') } }
+      let(:env) { entries.to_h }
       let(:env_data) { IO.read(test_data_file) }
 
       let(:expected_json) { File.read("#{test_data_file}.json") }
 
-      it { is_expected.to eq JSON.parse(expected_json) }
+      it { is_expected.to eq(JSON.parse(expected_json)) }
     end
   end
 
   context 'with no CI environment' do
-    subject { JSON.parse(ci_environment.to_json) }
-
-    let(:ci_environment) { Cucumber::CiEnvironment.detect_ci_environment(env) }
     let(:env) { {} }
 
     it { is_expected.to be_nil }
