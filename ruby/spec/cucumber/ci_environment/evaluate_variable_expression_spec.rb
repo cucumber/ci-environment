76c324db25cabfc89e051e7de0076518191a8a2d
Luke Hill <lukehill_uk@hotmail.com>
2024-11-05 10:40:32 +0000

    AF: StringLiteral

--- ruby/spec/cucumber/ci_environment/evaluate_variable_expression_spec.rb
+++ ruby/spec/cucumber/ci_environment/evaluate_variable_expression_spec.rb
@@ -4,13 +4,13 @@ describe 'Cucumber::CiEnvironment::VariableExpression.evaluate' do
   include Cucumber::CiEnvironment::VariableExpression
 
   it 'returns nil when a variable is undefined' do
-    expression = "hello-${SOME_VAR}"
+    expression = 'hello-${SOME_VAR}'
     result = evaluate(expression, {})
     expect(result).to eq(nil)
   end
 
   it 'gets a value without replacement' do
-    expression = "${SOME_VAR}"
+    expression = '${SOME_VAR}'
     result = evaluate(expression, { 'SOME_VAR' => 'some_value' })
     expect(result).to eq('some_value')
   end
@@ -22,13 +22,13 @@ describe 'Cucumber::CiEnvironment::VariableExpression.evaluate' do
   end
 
   it 'works with star wildcard in var' do
-    expression = "${GO_SCM_*_PR_BRANCH/.*:(.*)/\\1}"
+    expression = '${GO_SCM_*_PR_BRANCH/.*:(.*)/\\1}'
     result = evaluate(expression, { 'GO_SCM_MY_MATERIAL_PR_BRANCH' => 'ashwankthkumar:feature-1' })
     expect(result).to eq('feature-1')
   end
 
   it 'evaluates a complex expression' do
-    expression = "hello-${VAR1}-${VAR2/(.*) (.*)/\\2-\\1}-world"
+    expression = 'hello-${VAR1}-${VAR2/(.*) (.*)/\\2-\\1}-world'
     result = evaluate(expression, {
       'VAR1' => 'amazing',
       'VAR2' => 'gorgeous beautiful'
