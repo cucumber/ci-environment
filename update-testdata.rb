02bbb9942053284848fd379741a5f4def85bcdaf
Luke Hill <lukehill_uk@hotmail.com>
2024-11-07 09:54:15 +0000

    Tidy up update script

--- update-testdata.rb
+++ update-testdata.rb
@@ -1,26 +1,27 @@
+# frozen_string_literal: true
+
 require 'json'
 require 'set'
 
 # This script can be used to update the test data when the CiEnvironments.json file is modified.
 # After running this script, the testdata should be inspected and updated manually
 
-ciDict = JSON.parse(File.read(File.dirname(__FILE__) + '/CiEnvironments.json'))
+json = JSON.parse(File.read("#{File.dirname(__FILE__)}/CiEnvironments.json"))
 
-def collect_vars(ob, env_vars)
-  ob.each do |key, val|
-    if val.is_a?(String)
-      # RegExp copied from ruby/lib/cucumber/create_meta/variable_expression
-      # Collect all variables
-      val.scan(/\${(.*?)(?:(?<!\\)\/(.*)\/(.*))?}/) do |match|
+def collect_vars(environment, env_vars)
+  environment.each_value do |value|
+    if value.is_a?(String)
+      # Collect all variables: RegExp copied from ruby/lib/cucumber/ci_environment/variable_expression.rb
+      value.scan(%r{\${(.*?)(?:(?<!\\)/(.*)/(.*))?}}) do |match|
         env_vars.add(match[0])
       end
-    elsif val
-      collect_vars(val, env_vars)
+    elsif value
+      collect_vars(value, env_vars)
     end
   end
 end
 
-ciDict.each do |name, ci|
+json.each do |name, ci|
   filename = "testdata/#{name.gsub(/\s/, '')}.txt"
   puts "--- #{filename}"
   txt_file_env_vars = []
@@ -32,15 +33,11 @@ ciDict.each do |name, ci|
   used_env_vars = Set.new
   collect_vars(ci, used_env_vars)
   used_env_vars.to_a.sort.each do |used_env_var|
-    exists = txt_file_env_vars.detect { |pair| pair[0] == used_env_var}
-    if !exists
-      txt_file_env_vars.push([used_env_var, '???'])
-    end
+    exists = txt_file_env_vars.detect { |pair| pair[0] == used_env_var }
+    txt_file_env_vars.push([used_env_var, '???']) unless exists
   end
-  File.open(filename, "w:UTF-8") do |io|
-    txt = txt_file_env_vars.map do |pair|
-      pair.join('=')
-    end.join("\n") + "\n"
+  File.open(filename, 'w:UTF-8') do |io|
+    txt = txt_file_env_vars.map { |pair| pair.join('=') }.join("\n") + "\n"
     io.write(txt)
   end
 end
